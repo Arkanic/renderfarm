9bb053019ca61fda18f78368ac45b8bf4936bc8f
Arkanic <silverfishasaurus@gmail.com>
2023-03-04 02:52:39 +0000

    server does not know render dimensions

--- worker/worker/renderer.py
+++ worker/worker/renderer.py
@@ -4,23 +4,9 @@ import sys
 
 # 0 = directory to render to
 # 1 = frame to render
-# 2 = min x border
-# 3 = max x border
-# 4 = min y border
-# 5 = max y border
-# 6 = frame width
-# 7 = frame height
-
-def translate(value, leftMin, leftMax, rightMin, rightMax):
-    # figure out width
-    leftSpan = leftMax - leftMin
-    rightSpan = rightMax - rightMin
-
-    # Convert the left range into a 0-1 range (float)
-    valueScaled = float(value - leftMin) / float(leftSpan)
-
-    # Convert the 0-1 range into a value in the right range.
-    return rightMin + (valueScaled * rightSpan)
+# 2 = split into how many per side?
+# 3 = row to render
+# 4 = column to render
 
 argv = sys.argv
 argv = argv[argv.index("--") + 1:]
@@ -35,10 +21,20 @@ rndr.filepath = os.path.join(argv[0], "out")
 
 scene.frame_set(int(argv[1]))
 
-# set border (border range is between 0 and 1, we were provided 0-framesize so need to recalculate)
-rndr.border_min_x = translate(int(argv[2]), 0, rndr.resolution_x, 0, 1)
-rndr.border_max_x = translate(int(argv[3]), 0, rndr.resolution_x, 0, 1)
-rndr.border_min_y = translate(int(argv[4]), 0, rndr.resolution_y, 0, 1)
-rndr.border_max_y = translate(int(argv[5]), 0, rndr.resolution_y, 0, 1)
+# set boundaries for render
+cut_into = int(argv[2])
+row = int(argv[3])
+column = int(argv[4])
+
+rndr.border_min_x = (1 / cut_into) * row
+rndr.border_max_x = (1 / cut_into) * (row + 1)
+rndr.border_min_y = (1 / cut_into) * column
+rndr.border_max_y = (1 / cut_into) * (column + 1)
 
 bpy.ops.render.render(write_still = True)
+
+# blender -b file.blend -P renderer.py -noaudio -- "//render" 0 2 0 1
+#                                                   path      frame
+#                                                               cut into how many?
+#                                                                 row
+#                                                                   column
