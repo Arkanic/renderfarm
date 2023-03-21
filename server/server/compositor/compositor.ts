2948bf83f988068c9a719dccf69d7547df21dd95
Arkanic <silverfishasaurus@gmail.com>
2023-03-21 07:52:00 +0000

    render a single image

--- server/server/compositor/compositor.ts
+++ server/server/compositor/compositor.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 
+import {createCanvas, loadImage} from "node-canvas";
+
 import {Context} from "../server";
 import constants from "../constants";
 
@@ -26,4 +28,20 @@ function getImagesFormat(firstChunkid:string):string {
 export async function compositeRender(ctx:Context, projectid:string | number) {
     let project = await ctx.dbc.getById("projects", projectid);
     let renderdata = await ctx.dbc.getById("renderdata", project.renderdata_index);
+
+    let format = getImagesFormat(`${project.id}_${renderdata.framestart}_0_0`); // so we know if it is png or jpeg
+    let imagesPath = path.join(constants.DATA_DIR, constants.RENDERS_DIR, `${project.id}`, "raw");
+
+    let sampleImage = await loadImage(path.join(imagesPath, `${project.id}_${renderdata.framestart}_0_0.${format}`));
+    
+    let canvas = createCanvas(sampleImage.width, sampleImage.height);
+    let c = canvas.getContext("2d");
+    for(let row = 0; row < renderdata.cutinto; row++) {
+        for(let col = 0; col < renderdata.cutinto; col++) {
+            let image = await loadImage(path.join(imagesPath, `${project.id}_${renderdata.framestart}_${row}_${col}.${format}`));
+            c.drawImage(image, 0, 0);
+        }
+    }
+
+    fs.writeFileSync(path.join(constants.DATA_DIR, constants.RENDERS_DIR, `${project.id}`, "raw", `${renderdata.framestart}.PNG`), canvas.toBuffer());
 }
