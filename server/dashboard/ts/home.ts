ca30854c079931f226eb4c26e4a0c56e1b0b1503
Arkanic <50847107+Arkanic@users.noreply.github.com>
2023-03-31 07:22:07 +1300

    don't show message box if there isn't a message in the first place

--- server/dashboard/ts/home.ts
+++ server/dashboard/ts/home.ts
@@ -37,7 +37,7 @@ export default async function home() {
         title.innerHTML = project.title;
         section.appendChild(title);
 
-        if(project.finished) {
+        if(project.finished && project.message) {
             let messagesBox = document.createElement("div");
             messagesBox.classList.add("terminal");
 
