2d96d853a0bf485f448b7cbcc88c30fe1e4045ef
Arkanic <silverfishasaurus@gmail.com>
2023-04-05 00:35:19 +0000

    generate password

--- server/server/constants.ts
+++ server/server/constants.ts
@@ -1,5 +1,5 @@
 export default {
-    VERSION: "1.1.0",
+    VERSION: "1.1.6",
     DATA_DIR: "data",
     PROJECTS_DIR: "projects",
     RENDERS_DIR: "renders",
@@ -7,5 +7,6 @@ export default {
     HEARTBEAT_INTERVAL: 1000 * 60, // one minute
     TASK_WAIT_TIME: 1000 * 60, // one minute
     MAX_PROJ_ERRORS: 15, // maximum errors that can occur from a project before ditching it,
-    WORKER_LOG_NEWLINE_LENGTH: 50 // remember the previous 50 lines of the log per worker
+    WORKER_LOG_NEWLINE_LENGTH: 50, // remember the previous 50 lines of the log per worker
+    BCRYPT_SALT_ROUNDS: 16
 }
