fa5e31bf84069f6db639c16ca36af9fc77b1952e
Arkanic <silverfishasaurus@gmail.com>
2023-04-27 08:30:54 +0000

    delete project thumbnail upon project delete

--- server/server/routes/orchestrator.ts
+++ server/server/routes/orchestrator.ts
@@ -265,6 +265,7 @@ export default (ctx:Context) => {
         console.log(`Deleting project ${projects[0].title}`);
         fs.rmSync(path.join(constants.DATA_DIR, constants.PROJECTS_DIR, `${req.body.projectid}.zip`));
         fs.rmSync(path.join(constants.DATA_DIR, constants.RENDERS_DIR, `${req.body.projectid}`), {recursive: true, force: true});
+        fs.rmSync(path.join(constants.DATA_DIR, constants.THUMBNAIL_DIR, `${req.body.projectid}.jpg`));
 
         await dbc.deleteById("projects", req.body.projectid);
 
