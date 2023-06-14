1b2557ba7d7d2b4a052fdf00ac2e47ffddfd2424
Arkanic <silverfishasaurus@gmail.com>
2023-06-14 20:04:27 +0000

    basic, no autoscroll etc

---
+++ server/dashboard/ts/server.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import {apiurl, networkOptions} from "./networking";
+import {autoUpdate} from "./util/autoupdate";
+import * as types from "./types/api";
+
+const UPDATE_RATE = 5; // 5 seconds
+
+let serverLogs = document.getElementById("server-logs")!;
+let serverUpdateText = document.getElementById("server-update-text")!;
+let serverUpdateButton = document.getElementById("server-update-button")! as HTMLInputElement;
+
+export default async function server() {
+    await serverTask();
+
+    autoUpdate(serverUpdateText, serverUpdateButton, UPDATE_RATE, async () => {
+        await serverTask();
+    });
+}
+
+async function serverTask() {
+    let logs:types.ServerLogResponse = (await axios.post(`${apiurl()}/api/serverlog`, {}, networkOptions())).data;
+    
+    serverLogs.innerHTML = "";
+
+    for(let i in logs.logs) {
+        let log = logs.logs[i];
+        let part = document.createElement("p");
+        part.innerHTML = log;
+        serverLogs.appendChild(part);
+    }
+}
